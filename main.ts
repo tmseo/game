ac06e9d919ec69f59d2dab52eb7b2807f2fe2ec5
tmseo <121499395+tmseo@users.noreply.github.com>
2022-12-27 22:28:35 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,82 @@
+input.onButtonPressed(Button.A, function () {
+    game.startCountdown(1000)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        # # # # #
+        # . . . .
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        . . . . #
+        . . . . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        . . . . #
+        . . . . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # # # # #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # . # # #
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # # #
+        `)
+})
